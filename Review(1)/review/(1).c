58ad5a6e2ac8dd92dba1d3e4cca6ee8fa1064fa6
RepublicHo <75005879+RepublicHo@users.noreply.github.com>
2021-01-24 10:28:50 +0800

    Add files via upload
    Introduction to C and C++, composed of Escape characters \ and stream extraction operator.

---
+++ Review(1)/review/(1).c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+int main()
+{
+	printf("Hello world\n");
+	
+	/*	data type
+		char        //字符数据类型
+		short       //短整型
+		int         //整形
+		long        //长整型
+		long long   //更长的整形
+		float       //单精度浮点数
+		double      //双精度浮点数*/
+	
+	// To verify how long each type is
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	/* 在C++中并未规定int long，long long的具体字节数
+	* 但可以确定的是 int<=long<=long long
+	*/
+	
+	// 定义变量的方法(当局部变量has the same name as全局变量
+	// local variabel takes precedence
+	int age;//› Declare variables with name and data type before use 
+	float weight = 45.5F;
+	printf("weight is %f", weight);
+	/* 
+	数字后加符号
+	U由无符号整形储存
+	L由长整型储存（先区别数字，再分为长整形和logn double）
+	F代表由浮点方式储存
+	默认十进制
+	H 该数字由十六进制储存
+	B 八进制
+	前加0 八进制 Octonary
+	前加0x 十六进制 Hexadecimal
+	*/
+	return 0;
+}
---
+++ Review(1)/review/(1).cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>//Improve program readability
+// <iostream>是C++的头文件库
+
+/* #include <filename> means:
+– To add the contents of the library file – filename to the source program during preprocessing
+– < > means to find the library file in the default location
+*/
+using namespace std;
+int main() {
+	cout << "Hello World!\n" << endl;  
+	// cout --> connected to scream
+	// << --> Stream insertion operator
+	// Value on the right-hand side (right operand) 
+	// inserted into output stream
+
+	/*
+	\n Newline
+	\t Horizontal tab Move the screen cursor to the next tab stop.
+	另一种解释是把当前字符串长度补全到8的整数倍
+	\r Carriage return  回车
+	\a Alert 响铃
+	\\ used to print a backslash character
+	\" used to print a double quote character
+	*/
+
+	// Can declare several variables of the same type in one declaration
+
+	// >> (stream extraction operator 流提取运算符
+	// cin C++ 中标准输入流对象
+	int integer1,integer2,sum;
+	double average;
+	cout << "Enter first integer:";
+	cin >> integer1;
+	cout << "Enter second integer:";
+	cin >> integer2;
+	sum = integer1 + integer2;
+	average = sum / 3.0;
+	cout << "The sum is " << sum << endl;
+	cout << "The average is " << average << endl;
+	return 0;
+	// indicate that program ended successfully
+}
