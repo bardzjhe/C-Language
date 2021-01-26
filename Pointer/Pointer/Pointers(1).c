842ee464c452276a620f0da5329cd0c940044566
Ryan Che-tsun HO <75005879+RepublicHo@users.noreply.github.com>
2021-01-26 22:52:55 +0800

    Add files via upload

---
+++ Pointer/Pointer/Pointers(1).c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+int main()
+{
+	double a, * pd;
+	char b;
+	void *data;
+	/*At this point, the variable pd does not yet have a defined value*/
+	a = 3.14;
+	pd = &a;
+	/*We assign it a value by means of the reference operator &, which is a unary operator returning
+	the address of the operand on its right*/
+	// After assignment the variable pd, occupying 4 byte on a 32 bit architecture, contains the address the memory location
+	printf("the address of a is %p\n", &pd);
+	b = "Hello world";
+	data = &b;
+	
+	printf("%s\n", &data);// 有問題
+
+	// It's possible to define a pointer to the void type 無類型變量
+	return 0;
+}
